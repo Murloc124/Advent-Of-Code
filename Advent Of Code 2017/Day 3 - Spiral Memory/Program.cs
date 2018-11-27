cdee63cd03da252a48e816bc39d64ccf574577cf
Cédric Ally <cally@icorda.be>
2018-11-27 17:26:12 +0100

    Voorlopige versie dag 3

---
+++ Advent Of Code 2017/Day 3 - Spiral Memory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3___Spiral_Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = 347991;
+            var grid = 1;
+            var result = 0;
+
+            while ((grid * grid) <= data)
+            {
+                grid = grid + 2;
+            }
+
+            if (data - 591 < )
+
+
+            Console.WriteLine("Spiral Memory Part 1: " + result);
+            Console.Read();
+        }
+    }
+}
