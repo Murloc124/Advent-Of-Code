fbc655f18db5b5ecfbd0cce64a5780d587ed3e92
Cédric Ally <cally@icorda.be>
2018-11-28 12:08:56 +0100

    Solved Day3 - Spiral Memory

--- Advent Of Code 2017/Day 3 - Spiral Memory/Program.cs
+++ Advent Of Code 2017/Day 3 - Spiral Memory/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day_3___Spiral_Memory
 {
@@ -14,15 +11,140 @@ namespace Day_3___Spiral_Memory
             var grid = 1;
             var result = 0;
 
-            while ((grid * grid) <= data)
+            while ((grid * grid) < data)
             {
                 grid = grid + 2;
             }
-
-            if (data - 591 < )
-
+            
+            if (grid * grid - data < grid) //1st arm
+            {
+                if ((grid * grid) - (data) < grid / 2)
+                {
+                    result = grid - 1 - ((grid * grid) - data);
+                } else
+                    result = ((grid * grid) - data);
+            } else if (grid <= (grid * grid) - data && (grid * grid) - data < (grid - 1) * 2) //2nd arm
+            {
+                if (((grid * grid) - (data + grid)) < grid / 2)
+                {
+                    result = grid - 1 - ((grid * grid) - (data + grid - 1));
+                } else
+                    result = ((grid * grid) - (data + grid)) + 1;
+            } else if ((grid - 1) * 2 <= (grid * grid) - data && (grid * grid) - data < (grid - 1) * 3) //3rd arm
+            {
+                if (((grid * grid) - (data + (2 * (grid - 1)))) < grid / 2)
+                {
+                    result = grid - 1 - ((grid * grid) - (data + 2 * (grid - 1)));
+                } else 
+                    result = ((grid * grid) - (data + 2 * (grid - 1)));
+            } else if ((grid - 1) * 3 <= (grid * grid) - data && (grid * grid) - data < (grid - 1) * 4) //4th arm
+            {
+                if (((grid * grid) - (data + (3 * (grid - 1)))) < grid / 2)
+                {
+                    result = grid - 1 - ((grid * grid) - (data + 3 * (grid - 1)));
+                } else
+                    result = (grid * grid) - (data + 3 * (grid - 1));
+            }
 
             Console.WriteLine("Spiral Memory Part 1: " + result);
+
+            grid = 1;
+            result = 0;
+
+            var map = new Dictionary<int[], int>();
+            var key = new int[] { 0, 0 };
+            map.Add(key, 1);
+
+            while (result < data)
+            {
+                result = 0;
+                var newkey = new int[] { key[0], key[1] };
+                if (newkey[0] == newkey[1] && newkey[0] != -grid) //Init move right
+                {
+                    newkey[1]++;
+                    foreach (var mapKey in map.Keys)
+                    {
+                        if ((mapKey[0] - 1 == newkey[0]
+                            || mapKey[0] == newkey[0]
+                            || mapKey[0] + 1 == newkey[0]) &&
+                            (mapKey[1] - 1 == newkey[1]
+                            || mapKey[1] == newkey[1]
+                            || mapKey[1] + 1 == newkey[1]))
+                        {
+                            result += map[mapKey];
+                        }
+                    }
+                } else if (newkey[0] < newkey[1] && newkey[0] != -grid) //move to the top
+                {
+                    newkey[0]--;
+                    foreach (var mapKey in map.Keys)
+                    {
+                        if ((mapKey[0] - 1 == newkey[0]
+                            || mapKey[0] == newkey[0]
+                            || mapKey[0] + 1 == newkey[0]) &&
+                            (mapKey[1] - 1 == newkey[1]
+                            || mapKey[1] == newkey[1]
+                            || mapKey[1] + 1 == newkey[1]))
+                        {
+                            result += map[mapKey];
+                        }
+                    }
+                } else if (newkey[0] < newkey[1] && newkey[1] != -grid) //move to the left
+                {
+                    newkey[1]--;
+                    foreach (var mapKey in map.Keys)
+                    {
+                        if ((mapKey[0] - 1 == newkey[0]
+                            || mapKey[0] == newkey[0]
+                            || mapKey[0] + 1 == newkey[0]) &&
+                            (mapKey[1] - 1 == newkey[1]
+                            || mapKey[1] == newkey[1]
+                            || mapKey[1] + 1 == newkey[1]))
+                        {
+                            result += map[mapKey];
+                        }
+                    }
+                } else if (newkey[0] >= newkey[1] && newkey[1] == -grid && newkey[0] != grid) //move down
+                {
+                    newkey[0]++;
+                    foreach (var mapKey in map.Keys)
+                    {
+                        if ((mapKey[0] - 1 == newkey[0]
+                            || mapKey[0] == newkey[0]
+                            || mapKey[0] + 1 == newkey[0]) &&
+                            (mapKey[1] - 1 == newkey[1]
+                            || mapKey[1] == newkey[1]
+                            || mapKey[1] + 1 == newkey[1]))
+                        {
+                            result += map[mapKey];
+                        }
+                    }
+                } else if (newkey[0] > newkey[1] && newkey[0] == grid) //move right
+                {
+                    newkey[1]++;
+                    foreach (var mapKey in map.Keys)
+                    {
+                        if ((mapKey[0] - 1 == newkey[0]
+                            || mapKey[0] == newkey[0]
+                            || mapKey[0] + 1 == newkey[0]) &&
+                            (mapKey[1] - 1 == newkey[1]
+                            || mapKey[1] == newkey[1]
+                            || mapKey[1] + 1 == newkey[1]))
+                        {
+                            result += map[mapKey];
+                        }
+                    }
+                }
+
+                if (grid == newkey[0] && grid == newkey[1])
+                {
+                    grid++;
+                }
+                key = newkey;
+                map.Add(newkey, result);
+            }
+
+            Console.WriteLine("Spiral Memory Part 2: " + result);
             Console.Read();
         }
     }
