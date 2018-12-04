ae3ee63f07efeb324a2a1b94750ede7f3ff739f0
Cédric Ally <cally@icorda.be>
2018-12-04 09:35:38 +0100

    Day 04 Completed

---
+++ Advent Of Code 2018/Day 04 - Repose Record/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_04___Repose_Record
+{
+    class Program
+    {
+        private static List<string> Data = File.ReadAllLines(@"..\..\Day04-Data.txt").ToList();
+        private static Dictionary<string, int> Guards = new Dictionary<string, int>();
+        private static Dictionary<int, int> MinutesAsleepForSleeperGuard = new Dictionary<int, int>();
+        private static Dictionary<int, Dictionary<int, int>> GuardsSleepyMinutes = new Dictionary<int, Dictionary<int, int>>();
+
+        static void Main(string[] args)
+        {
+            Data.Sort();
+            Data.ForEach(x =>
+            {
+                if (x.Contains("#"))
+                    if(!Guards.ContainsKey(x.Substring(x.IndexOf('#') + 1, x.IndexOf('b') - (x.IndexOf('#') + 1)).Replace(" ", "")))
+                        Guards.Add(x.Substring(x.IndexOf('#') + 1, x.IndexOf('b') - (x.IndexOf('#') + 1)).Replace(" ", ""), 0);
+            });
+            string guardID = "";
+            bool asleep = false;
+            int minuteFellAsleep = 0;
+            Data.ForEach(x =>
+            {
+                if (x.Contains("#"))
+                    guardID = x.Substring(x.IndexOf('#') + 1, x.IndexOf('b') - (x.IndexOf('#') + 1)).Replace(" ", "");
+                else
+                {
+                    if (!asleep)
+                    {
+                        minuteFellAsleep = int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1)));
+                        asleep = true;
+                    } else
+                    {
+                        Guards[guardID] += Math.Abs(minuteFellAsleep - int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1))));
+                        asleep = false;
+                    }
+                }
+            });
+            Data.ForEach(x =>
+            {
+                if (x.Contains("#"))
+                {
+                    guardID = x.Substring(x.IndexOf('#') + 1, x.IndexOf('b') - (x.IndexOf('#') + 1)).Replace(" ", "");
+                }
+                else if (guardID.Equals(Guards.Aggregate((l, r) => l.Value > r.Value ? l : r).Key))
+                {
+                    if (!asleep)
+                    {
+                        minuteFellAsleep = int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1)));
+                        asleep = true;
+                    } else
+                    {
+                        for (int i = minuteFellAsleep; i < int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1))); i++)
+                        {
+                            if (MinutesAsleepForSleeperGuard.ContainsKey(i))
+                                MinutesAsleepForSleeperGuard[i]++;
+                            else
+                                MinutesAsleepForSleeperGuard.Add(i, 1);
+                        }
+                        asleep = false;
+                    }
+                } else
+                {
+                    if (!asleep)
+                    {
+                        minuteFellAsleep = int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1)));
+                        asleep = true;
+                    }
+                    else
+                    {
+                        for (int i = minuteFellAsleep; i < int.Parse(x.Substring(x.IndexOf(':') + 1, x.IndexOf(']') - (x.IndexOf(':') + 1))); i++)
+                        {
+                            if (GuardsSleepyMinutes.ContainsKey(int.Parse(guardID)))
+                            {
+                                if (GuardsSleepyMinutes[int.Parse(guardID)].ContainsKey(i))
+                                    GuardsSleepyMinutes[int.Parse(guardID)][i]++;
+                                else
+                                    GuardsSleepyMinutes[int.Parse(guardID)].Add(i, 1);
+                            }
+                            else
+                            {
+                                GuardsSleepyMinutes.Add(int.Parse(guardID), new Dictionary<int, int>());
+                                GuardsSleepyMinutes[int.Parse(guardID)].Add(i, 1);
+                            }
+                        }
+                        asleep = false;
+                    }
+                }
+            });
+            var kvp = GuardsSleepyMinutes.Aggregate((l, r) => l.Value.Aggregate((val1, val2) => val1.Value > val2.Value ? val1 : val2).Value > r.Value.Aggregate((val1, val2) => val1.Value > val2.Value ? val1 : val2).Value ? l : r);
+
+            Console.WriteLine("Day 04 - Part 1: " + (int.Parse(Guards.Aggregate((l, r) => l.Value > r.Value ? l : r).Key) * MinutesAsleepForSleeperGuard.Aggregate((l, r) => l.Value > r.Value ? l : r).Key));
+            Console.WriteLine("Day 04 - Part 2: " + (kvp.Key * kvp.Value.Aggregate((l, r) => l.Value > r.Value ? l : r).Key));
+            Console.ReadLine();
+        }
+    }
+}
